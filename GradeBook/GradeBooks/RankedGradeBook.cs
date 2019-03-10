e8dc5c424b002a4f375f2937e67a43e02a1380e8
kieranmooreuk77 <48412024+kieranmooreuk77@users.noreply.github.com>
2019-03-10 16:22:57 +0000

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,48 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int threshold = (int)(this.Students.Count * 0.2);
+            int numLessThan = 0;
+            foreach (Student student in this.Students)
+            {
+                if (averageGrade < student.AverageGrade)
+                {
+                    numLessThan += 1;
+                }
+            }
+
+            char letterGrade;
+            if (numLessThan < threshold)
+            {
+                letterGrade = 'A';
+            }
+            else if (numLessThan < (threshold * 2))
+            {
+                letterGrade = 'B';
+            }
+            else if (numLessThan < (threshold * 3))
+            {
+                letterGrade = 'C';
+            }
+            else if (numLessThan < (threshold * 4))
+            {
+                letterGrade = 'D';
+            }
+            else
+            {
+                letterGrade = 'F';
+            }
+            
+            return letterGrade;
         }
     }
 }
